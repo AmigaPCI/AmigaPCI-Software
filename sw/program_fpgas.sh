14fc837cf5818268fcbc2014646baf5b5934b817
Chris Hooper <git@cdh.eebugs.com>
2026-07-24 14:08:09 -0700

    SW - Enhanced FPGA programming script for device and path selection - Added Amiga reset and power cycle options to FPGA programming script

--- sw/program_fpgas.sh
+++ sw/program_fpgas.sh
@@ -6,31 +6,60 @@
 #
 
 HOSTBEC=hostbec
-MB=../../AmigaPCI/Verilog/Release/BINs
-CPU=../../AmigaPCI-LBC040/Verilog/Release/BINs
-[ ! -d $MB ] && MB=~/projects/amigapci/AmigaPCI/Verilog/Release/BINs
-[ ! -d $CPU ] && CPU=~/projects/amigapci/AmigaPCI-LBC040/Verilog/Release/BINs
-if [ -z $DEV ]; then
-    DEV=`ls -1 /dev/ttyUSB* | tail -1`
-    if [[ ! -e $DEV ]]; then
-        echo 'No /dev/ttyUSB* device found'
-        exit 1
-    fi
+DEFAULT_MB_BINS=../../AmigaPCI/Verilog/Release/BINs
+DEFAULT_CPU_BINS=../../AmigaPCI-LBC040/Verilog/Release/BINs
+DEFAULT_DEV_PREFIX=/dev/ttyUSB
+PROGCONFIG=.progconfig
+[ ! -d $MB_BINS ] && MB_BINS=~/projects/amigapci/AmigaPCI/Verilog/Release/BINs
+[ ! -d $CPU_BINS ] && CPU_BINS=~/projects/amigapci/AmigaPCI-LBC040/Verilog/Release/BINs
+if [ -f $PROGCONFIG ]; then
+    [ -z $DEV ] && DEV=$(awk -F= '/^DEV/ { print $2 }' $PROGCONFIG)
+    [ -z $MB_BINS ] && MB_BINS=$(awk -F= '/^MB_BINS/ { print $2 }' $PROGCONFIG)
+    [ -z $CPU_BINS ] && CPU_BINS=$(awk -F= '/^CPU_BINS/ { print $2 }' $PROGCONFIG)
 fi
+
+if [ -z $DEV ]; then
+    echo "The following devices are attached: "
+    ls -1 "$DEFAULT_DEV_PREFIX"* | sed -e 's/^/    /'
+    while read -ei $DEFAULT_DEV_PREFIX -p "Enter device name to use: " DEV ; do
+        [ -e $DEV ] && break
+        echo "$DEV does not exist"
+    done
+fi
+
+if [ -z $MB_BINS ]; then
+    while read -ei $DEFAULT_MB_BINS -p "Enter path to MB FPGA binaries: " MB_BINS ; do
+        [ -d $MB_BINS"/" ] && break
+        echo "$MB_BINS does not exist"
+    done
+fi
+
+if [ -z $CPU_BINS ]; then
+    while read -ei $DEFAULT_CPU_BINS -p "Enter path to CPU FPGA binaries: " CPU_BINS ; do
+        [ -d $CPU_BINS"/" ] && break
+        echo "$CPU_BINS does not exist"
+    done
+fi
+
+(
+    echo "DEV="$DEV
+    echo "MB_BINS="$MB_BINS
+    echo "CPU_BINS="$CPU_BINS
+)> $PROGCONFIG
 
 declare -A FILES
 
-FILES[1,0]=$MB/U110_TOP_bitmap.bin
+FILES[1,0]=$MB_BINS/U110_TOP_bitmap.bin
 FILES[1,1]=0,0
-FILES[2,0]=$MB/U109_TOP_bitmap.bin
+FILES[2,0]=$MB_BINS/U109_TOP_bitmap.bin
 FILES[2,1]=1,0
-FILES[3,0]=$MB/U712_TOP_bitmap.bin
+FILES[3,0]=$MB_BINS/U712_TOP_bitmap.bin
 FILES[3,1]=2,0
-FILES[4,0]=$MB/U409_TOP_bitmap.bin
+FILES[4,0]=$MB_BINS/U409_TOP_bitmap.bin
 FILES[4,1]=3,0
-FILES[5,0]=$CPU/U111_TOP_bitmap.bin
+FILES[5,0]=$CPU_BINS/U111_TOP_bitmap.bin
 FILES[5,1]=4,0
-FILES[6,0]=$CPU/U400_TOP_bitmap.bin
+FILES[6,0]=$CPU_BINS/U400_TOP_bitmap.bin
 FILES[6,1]=5,0
 
 do_cmd() {
@@ -49,7 +78,14 @@ show_files() {
         echo "    $i  $SPI  $SHORTNAME  $DTIME"
     done
     echo "    A       Program All"
+    echo "    R       Reset Amiga"
+    echo "    P       Power Cycle Amiga"
     echo "    X       Exit"
+}
+
+bec_state() {
+    $HOSTBEC -d $DEV -t amiga status |
+    awk 'BEGIN { SAW=0 } /Power state/ { print "    " $0; SAW=1 } END { if (SAW == 0) print "    WARNING: No BEC response at this device" }'
 }
 
 program_single() {
@@ -59,7 +95,7 @@ program_single() {
     SHORTNAME="${FILEPATH##*/}"
     SHORTERNAME="${SHORTNAME%%_*}"
     echo "------------------------ $IDX $SHORTERNAME ------------------------"
-    do_cmd hostbec -d $DEV -wvy -a 0,0 $FILEPATH
+    do_cmd $HOSTBEC -d $DEV -wvy -a 0,0 $FILEPATH
 }
 
 program_all() {
@@ -68,14 +104,29 @@ program_all() {
     done
 }
 
+reset_amiga() {
+    $HOSTBEC -d $DEV -t reset amiga
+}
+
+power_cycle_amiga() {
+    $HOSTBEC -d $DEV -t power cycle
+}
+
+show_menu()
+{
+    echo "MB_BINS=$MB_BINS"
+    echo "CPU_BINS=$CPU_BINS"
+    echo "DEV=$DEV"
+    bec_state
+    echo
+    show_files
+}
 
 NUM_FILES=${#FILES[@]}
 NUM_FILES=$((NUM_FILES / 2))
 NUM_FILES_MINUS_ONE=$((NUM_FILES - 1))
 
-echo "Using DEV=$DEV for programming"
-echo
-show_files
+show_menu
 while read -p "Enter file number to program: " WHICH; do
     case "$WHICH" in
         [1-$NUM_FILES])
@@ -87,18 +138,17 @@ while read -p "Enter file number to program: " WHICH; do
         [qQxX])
             exit 0
             ;;
+        [rR])
+            reset_amiga
+            ;;
+        [pP])
+            power_cycle_amiga
+            ;;
+        "")
+            ;;
         *)
             echo "Invalid option \"$WHICH\""
             ;;
     esac
+    show_menu
 done
-
-# Need to do Do U110 again
-
-#do_cmd U110 hostbec -d $DEV -wvy -a 0,0 $MB/U110_TOP_bitmap.bin &&
-#do_cmd U109 hostbec -d $DEV -wvy -a 1,0 $MB/U109_TOP_bitmap.bin &&
-#do_cmd U712 hostbec -d $DEV -wvy -a 2,0 $MB/U712_TOP_bitmap.bin &&
-#do_cmd U409 hostbec -d $DEV -wvy -a 3,0 $MB/U409_TOP_bitmap.bin &&
-#do_cmd U111 hostbec -d $DEV -wvy -a 4,0 $CPU/U111_TOP_bitmap.bin &&
-#do_cmd U400 hostbec -d $DEV -wvy -a 5,0 $CPU/U400_TOP_bitmap.bin &&
-echo Done
